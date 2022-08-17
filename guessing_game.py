0b379818ada51c1cc9074e69378c97d44e36211f
schmam <35980147+schmam@users.noreply.github.com>
2022-08-17 13:51:47 -0500

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,9 +1,7 @@
-# Guessing game - Asks user to provide a "level." CPU will pick a random number between 1 and the value of the level specified by the user. User then attempts to guess the number.
-
 import random
 import sys
 
-user_level = input("Level: ")                                       # prompts user to enter a number to be the upper bound of the random number to be guessed
+user_level = input("Level: ")                                       # the number entered by the user as the upper bound of the number to be guessed
 
 while True:
     if user_level.isnumeric() == True and int(user_level) > 0:
@@ -15,16 +13,13 @@ while True:
         continue
 
 while True:
-    user_guess = input("Guess: ")                                       # prompts user for their first guess
-    if user_guess.isnumeric() == True and int(user_guess) > 0:  # validates guess if a positive number
-        if int(user_guess) == cpu_guess:
-            print("Just right!")
-            sys.exit()                                          # exits once the user guesses the correct number
-        elif int(user_guess) > cpu_guess:
-            print("Too large!")
-            continue
-        elif int(user_guess) < cpu_guess:
-            print("Too small!")
-            continue
-        else:
-            continue
+    user_guess = input("Guess: ")
+    if int(user_guess) == cpu_guess:
+        print("Just right!")
+        break
+    elif int(user_guess) > cpu_guess:
+        print("Too large!")
+        continue
+    elif int(user_guess) < cpu_guess:
+        print("Too small!")
+        continue
